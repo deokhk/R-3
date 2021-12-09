0a1b9f3fefe3ef47e07fabb88f70e56f9bfb6d4c
deokhk <deokhk@postech.ac.kr>
2021-12-09 22:03:51 +0900

    for remove crash

--- model/FiD/cluster_run.sh
+++ model/FiD/cluster_run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH -J 64_multi_test # job name
+#SBATCH -J 64_new_linear_train_and_test # job name
 #SBATCH -o sbatch_output_log/output_%x_%j.out # standard output and error log
-#SBATCH -p A100 # queue name or partiton name
+#SBATCH -p 4A100 # queue name or partiton name
+#SBATCH -q 4A100
 #SBATCH -t 72:00:00 # Run time (hh:mm:ss)
-#SBATCH  --gres=gpu:1
+#SBATCH  --gres=gpu:4
 #SBATCH  --nodes=1
-#SBATCH  --ntasks=1
-#SBATCH  --tasks-per-node=1
-#SBATCH  --cpus-per-task=32
-#SBATCH  --mem=200G
+#SBATCH  --ntasks=8
+#SBATCH  --cpus-per-task=4
+#SBATCH  --mem=300G
 
 srun -l /bin/hostname
 srun -l /bin/pwd
@@ -19,6 +19,6 @@ module purge
 
 date
 
-sh test_reader.sh
+sh train_and_test_reader.sh
 
 date
